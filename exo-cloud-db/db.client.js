72d51b559f11f01f45d92ee138dd07e5269635bd
Doothrat <60654806+Doothrat@users.noreply.github.com>
2022-01-14 10:39:30 +0100

    Update db.client.js

--- exo-cloud-db/db.client.js
+++ exo-cloud-db/db.client.js
@@ -1,16 +1,20 @@
 const { Sequelize } = require('sequelize')
-
 // database
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'database.sqlite'
-});
-
+const sequelize = new Sequelize(
+User ID=yskhbdzkaofvnu;Password=80ab79aaf998ab96ab66776ac2fb8c6b552bfd7c0bf08c9d52df21444ed11a8a;Host=ec2-54-229-47-120.eu-west-1.compute.amazonaws.com;Port=5432;Database=d78o9bo0jh7o52;Pooling=true;Min Pool Size=0;Max Pool Size=100;Connection Lifetime=0;,
+{
+dialectOptions: {
+ssl: {
+require: true,
+rejectUnauthorized: false,
+},
+},
+},
+);
 // authentication and synchronization
 sequelize.authenticate()
-  .then(() => {
-    sequelize.sync().catch(() => console.log("Cannot sync the database"));
-  })
-  .catch(() => console.log("Cannot connect to database, please check environment credentials"));
-
+.then(() => {
+sequelize.sync().catch(() => console.log("Cannot sync the database"));
+})
+.catch(() => console.log("Cannot connect to database, please check environment credentials"));
 module.exports = sequelize;
